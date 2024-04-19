0b5eac4cd4e11b983c7c960beb5f9efddba4d189
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-04-18 22:20:36 -0500

    Add post method and html for a basic form

---
+++ submit.php
@@ -0,0 +1,14 @@
+<?php
+    // // check if form's submit is clicked
+    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //     // ensure form field name is not empty
+    //     if (!empty($_POST["name"])) {
+    //         $name = $_POST["name"]; // getting submitted data from form field
+    //         echo "Hello, " . htmlspecialchars($name) . "! Welcome to our site."; // display greeting and sanitize input
+    //     } else {
+    //         // empty field please enter name
+    //         echo "Please enter your name";
+    //     }
+    // }
+
+?>
