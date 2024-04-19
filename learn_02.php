a8c7023b1e428f0e1b18499cd8b32ff144cbb00a
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-04-18 20:37:03 -0500

    test

--- learn_02.php
+++ learn_02.php
@@ -3,22 +3,35 @@
 
 // creating a file
 
-$filename = "newfile_02.txt";
+// $filename = "newfile_02.txt";
 
-// handling case where file name exists already
-if (file_exists($filename)) {
-    echo "The file $filename already exists!";
-} else {
-    $file = fopen($filename, "w"); // open for writing only
-    if ($file === false) {
-        echo "Error in opening new file";
-        exit();
-    }
-    $text = "Hello, this file was just created\n";
-    fwrite($file, $text);
-    fclose($file);
-    echo "File created and initial data written";
-}
+// // handling case where file name exists already
+// if (file_exists($filename)) {
+//     echo "The file $filename already exists!";
+// } else {
+//     $file = fopen($filename, "w"); // open for writing only
+
+//     if ($file === false) {
+//         echo "Error in opening new file";
+//         exit();
+//     }
+
+//     $text = "Hello, this file was just created\n";
+
+//     fwrite($file, $text);
+
+//     chmod($filename, 0644);
+
+//     fclose($file);
+
+//     echo "File created, file permissions set, and initial data written";
+// }
+
+
+$filename = "newfile_03.txt";
+
+if (file_exists($filename))
+
 
 
 
