24aa3fb60b74ef4b09ce9ad814f9632b01cc0b9a
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-04-18 20:11:43 -0500

    Change logic to include case where file already exists

--- learn_02.php
+++ learn_02.php
@@ -3,24 +3,22 @@
 
 // creating a file
 
-$filename = "newfile.txt";
-$file = fopen($filename, "w");
+$filename = "newfile_02.txt";
 
-if ($file == false) {
-    echo "Error opening new file";
-    exit();
-};
-
-
-$text = "Hello this is a test file.\n";
-fwrite($file, $text);
-
-$text = "Adding more text to the test file.\n";
-fwrite($file, $text);
-
-fclose($file);
-echo "file created and data written successfully.";
-
+// handling case where file name exists already
+if (file_exists($filename)) {
+    echo "The file $filename already exists!";
+} else {
+    $file = fopen($filename, "w"); // open for writing only
+    if ($file === false) {
+        echo "Error in opening new file";
+        exit();
+    }
+    $text = "Hello, this file was just created\n";
+    fwrite($file, $text);
+    fclose($file);
+    echo "File created and initial data written";
+}
 
 
 
